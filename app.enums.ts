cb8bb2f8089341286448ad660520337fece94c95
Obioma Anomnachi <onanomnachi@gmail.com>
2024-05-14 15:01:55 +0000

    Workflow Test 1

---
+++ app.enums.ts
@@ -0,0 +1,49 @@
+export enum NavScreensEnum {
+  DISCOVER = 'Discover',
+  BURSTS = 'Bursts',
+  PLAY = 'Play',
+  TRENDING = 'Trending',
+  MENU = 'Menu',
+  RECENTLY_ADDED = 'Recently Added',
+  MY_MEDIA = 'My Media',
+  HISTORY = 'History',
+  SUBSCRIPTIONS = 'Subscriptions',
+  LIKED_VIDEOS = 'Liked Videos',
+  SETTINGS = 'Settings',
+  ABOUT = 'About',
+  UPLOAD_VIDEO = 'Upload video',
+  UPLOAD_BURST = 'Upload burst'
+}
+
+export enum MenuNavScreensEnum {
+  DISCOVER = '#',
+  BURSTS = '#',
+  PLAY = '#',
+  TRENDING = '#',
+  MENU = '#',
+  RECENTLY_ADDED = '#',
+  MY_MEDIA = '#',
+  HISTORY = '#',
+  SUBSCRIPTIONS = '#',
+  LIKED_VIDEOS = '#',
+  SETTINGS = '#',
+  ABOUT = '#',
+  UPLOAD_VIDEO = '#',
+  UPLOAD_BURST = '#'
+}
+
+export enum WebViewUris {
+  DISCOVER = 'https://play.par.pw/videos/overview',
+  BURSTS = 'https://bursts.par.pw/',
+  BURSTS_DISCOVER = 'https://play.par.pw/videos/bursts',
+  TRENDING = 'https://play.par.pw/videos/trending',
+  RECENTLY_ADDED = 'https://play.par.pw/videos/recently-added',
+  MY_MEDIA = 'https://play.par.pw/my-library/videos?s=1',
+  HISTORY = 'https://play.par.pw/my-library/history/videos',
+  SUBSCRIPTIONS = 'https://play.par.pw/videos/subscriptions',
+  LIKED_VIDEOS = '#',
+  SETTINGS = '#',
+  ABOUT = 'https://parler.com/',
+  UPLOAD_VIDEO = 'https://play.par.pw/videos/upload#upload',
+  UPLOAD_BURST = 'https://play.par.pw/videos/upload?tag=bursts'
+}
